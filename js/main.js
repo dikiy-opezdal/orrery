5599270f1f40d132254d9b2cd02788a1f1cde843
dikiyOpezdal <135449631+dikiyOpezdal@users.noreply.github.com>
2024-10-05 12:17:28 +0300

    Basic lighting, object with texture rendering

--- js/main.js
+++ js/main.js
@@ -1,2 +1,64 @@
-'use strict';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 5);
+
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.target.set( 0, 0, 0 );
+controls.update();
+controls.enablePan = false;
+controls.enableDamping = true;
+
+const ambientLight = new THREE.AmbientLight(0x404040);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight.position.set(1, 1, 0).normalize();
+scene.add(directionalLight);
+
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load('./public/models/moon.jpg')
+
+const objLoader = new OBJLoader();
+
+objLoader.load(
+	'./public/models/moon.obj',
+	function (object) {
+        object.traverse(function (child) {
+            if (child.isMesh) {
+                child.material.map = texture;
+            }
+        })
+		scene.add(object);
+        console.log('Object loaded');
+	},
+	function (xhr) {
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+	},
+	function (error) {
+		console.log('An error happened');
+	}
+);
+
+window.onresize = function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+};
+
+function animate() {
+    controls.update();
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+animate();
